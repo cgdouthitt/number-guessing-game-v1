db5482ca851b6032d3eee777c2c29c43f85730c6
cgdouthitt <46352253+cgdouthitt@users.noreply.github.com>
2025-03-19 15:35:37 -0500

    Basic requirements implemented

--- guessing_game.py
+++ guessing_game.py
@@ -5,23 +5,48 @@ Project 1 - The Number Guessing Game
 """
 
 # Import the random module.
-
+import random
+# High score
+high_score = 0
 # Create the start_game function.
+def start_game():
 # Write your code inside this function.
 
 #   When the program starts, we want to:
 #   ------------------------------------
 #   1. Display an intro/welcome message to the player.
+  print("<=======  !!!Welcome to the number guessing game!!!  =======>")
 #   2. Store a random number as the answer/solution.
+  random_number = random.randint(1, 10)
 #   3. Continuously prompt the player for a guess.
 #     a. If the guess is greater than the solution, display to the player "It's lower".
 #     b. If the guess is less than the solution, display to the player "It's higher".
-
+  user_guesses = 0
+  while True:
+    input_number = input("Guess a number (1 - 10): ")
+    try:
+      guessed_int = int(input_number)
+      user_guesses += 1
+      if guessed_int < 1 or guessed_int > 10:
+        print("This number is outside the guess range.")
+      elif guessed_int > random_number:
+        print("It's lower\n")
+      elif guessed_int < random_number:
+        print("It's higher")
+      else:
+        print(f"You got it!! \nIt took you {user_guesses} attempts to guess the number. \n<=======  Thanks for playing!!  =======>")
+        break
+    except ValueError:
+      print("Please enter a number.")
+      continue
+    
 #   4. Once the guess is correct, stop looping, inform the user they "Got it"
 #      and show how many attempts it took them to get the correct number.
+    
 #   5. Let the player know the game is ending, or something that indicates the game is over.
 
 # ( You can add more features/enhancements if you'd like to. )
 
 
 # Kick off the program by calling the start_game function.
+start_game()
