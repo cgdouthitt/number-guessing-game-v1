8934209087780990e12ac6ec81342af307152285
cgdouthitt <46352253+cgdouthitt@users.noreply.github.com>
2025-03-21 09:03:08 -0500

    extra comments added

--- guessing_game.py
+++ guessing_game.py
@@ -51,14 +51,6 @@ def start_game():
     except ValueError:
       print("Please enter a number.")
       continue
-    
-#   4. Once the guess is correct, stop looping, inform the user they "Got it"
-#      and show how many attempts it took them to get the correct number.
-    
-#   5. Let the player know the game is ending, or something that indicates the game is over.
 
-# ( You can add more features/enhancements if you'd like to. )
-
-
-# Kick off the program by calling the start_game function.
+#   Start the game
 start_game()
