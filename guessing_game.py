5c4349493292f2dc039185a7ff10b8168b4871b9
cgdouthitt <46352253+cgdouthitt@users.noreply.github.com>
2025-03-21 08:52:51 -0500

    Extra features added

--- guessing_game.py
+++ guessing_game.py
@@ -16,11 +16,13 @@ def start_game():
 #   ------------------------------------
 #   1. Display an intro/welcome message to the player.
   print("<=======  !!!Welcome to the number guessing game!!!  =======>")
+  print("The high score is not set yet. Go for it!!")
 #   2. Store a random number as the answer/solution.
   random_number = random.randint(1, 10)
 #   3. Continuously prompt the player for a guess.
 #     a. If the guess is greater than the solution, display to the player "It's lower".
 #     b. If the guess is less than the solution, display to the player "It's higher".
+  high_score = 10
   user_guesses = 0
   while True:
     input_number = input("Guess a number (1 - 10): ")
@@ -30,12 +32,21 @@ def start_game():
       if guessed_int < 1 or guessed_int > 10:
         print("This number is outside the guess range.")
       elif guessed_int > random_number:
-        print("It's lower\n")
+        print("It's lower")
       elif guessed_int < random_number:
         print("It's higher")
       else:
-        print(f"You got it!! \nIt took you {user_guesses} attempts to guess the number. \n<=======  Thanks for playing!!  =======>")
-        break
+        print(f"You got it!! \nIt took you {user_guesses} attempts to guess the number. \n<=======  !!Thanks for playing!!  =======>")
+        play_again = input("Would you like to play again? (y/n): ")
+        if play_again.lower() == "y":
+          if high_score > user_guesses:
+            high_score = int(user_guesses)
+          print(f"The current high score is {high_score}")
+          user_guesses = 0
+          random_number = random.randint(1, 10)
+          continue
+        else:
+          break
     except ValueError:
       print("Please enter a number.")
       continue
